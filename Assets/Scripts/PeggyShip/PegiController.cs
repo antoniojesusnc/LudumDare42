56f54b2868713f403708412681386fd3286bd11c
Antonio Jesus <estopero88@gmail.com>
2018-08-11 04:36:23 +0200

    pegiShip basic Movement

---
+++ Assets/Scripts/PeggyShip/PegiController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PegiController : MonoBehaviour {
+
+    [SerializeField]
+    PegiInput _input;
+
+    [SerializeField]
+    float _speed;
+
+    [SerializeField]
+    float _orbitCoolDown;
+
+    int _currentOrbit = 1;
+    float _orbitTimeStamp;
+    float _currentSpeed;
+
+    PlanetController _planet;
+
+	void Start () {
+        _currentSpeed = _speed;
+
+        _planet = GameObject.FindGameObjectWithTag("Planet").GetComponent<PlanetController>();
+    }
+	
+	void LateUpdate () {
+        transform.position += _input.Momentum.x * _speed * transform.right* Time.deltaTime;
+        SetOrbitPosition();
+    }
+
+    private void SetOrbitPosition()
+    {
+        _orbitTimeStamp -= Time.deltaTime;
+        if (_input.Momentum.y != 0 && _orbitTimeStamp <= 0)
+        {
+
+            int newOrbit = _currentOrbit + (int)_input.Momentum.y;
+            newOrbit = Mathf.Clamp(newOrbit, 1, 3);
+            if(newOrbit != _currentOrbit)
+            {
+                _currentOrbit = newOrbit;
+                _orbitTimeStamp = _orbitCoolDown;
+            }
+        }
+
+        float orbitPosition = _planet.GetOrbitPosition(_currentOrbit);
+
+        Vector3 dirToPlanet = (transform.position - _planet.transform.position ).normalized;
+
+        transform.position =  dirToPlanet* orbitPosition;
+
+        transform.up = dirToPlanet;
+    }
+}
---
+++ Assets/Scripts/Planet/PlanetController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlanetController : MonoBehaviour {
+
+    [SerializeField]
+    float _surface;
+
+    [SerializeField]
+    float _orbit01;
+    [SerializeField]
+    float _orbit02;
+    [SerializeField]
+    float _orbit03;
+    
+
+    public float GetOrbitPosition(int orbit)
+    {
+        switch (orbit)
+        {
+            case 1: return _orbit01;
+            case 2: return _orbit02;
+            case 3: return _orbit03;
+        }
+        return _orbit01;
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireSphere(transform.position, _surface);
+        Gizmos.color = Color.black;
+        Gizmos.DrawWireSphere(transform.position, _orbit01);
+        Gizmos.DrawWireSphere(transform.position, _orbit02);
+        Gizmos.DrawWireSphere(transform.position, _orbit03);
+    }
+}
