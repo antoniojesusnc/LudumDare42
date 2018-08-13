5facf14c8ef737fc078b7e0be707d1ae55b6e579
Antonio Jesus <estopero88@gmail.com>
2018-08-13 19:29:10 +0200

    doing inventory interface

---
+++ Assets/Scripts/GUI/GUIInventory.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GUIInventory : MonoBehaviour {
+
+
+    [SerializeField]
+    GameObject _topBar;
+
+    [SerializeField]
+    List<GameObject> _keys;
+
+    [SerializeField]
+    List<TMPro.TextMeshProUGUI> text;
+
+    [SerializeField]
+    List<Image> _downBar;
+
+
+
+
+    List<GameObject> _topBarElements;
+
+    void Start () {
+        
+    }
+    
+    void Update () {
+        
+    }
+}
